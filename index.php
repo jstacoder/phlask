9cdc0ec50457ed3fc093705b3b5b1a20d9c6dbf3
Kyle J. Roux <kyle@level2designs.com>
2014-12-15 01:05:44 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,21 +3,16 @@ require 'lib/phlask.php';
 
 $app = new Phlask('app');
 $app->config_from_file("settings.php");
-function index($app)
-{
+$index = function ($app) {
     return $app->render("new.html",array(
-        'testing' => true,
+        'testing' => true
     ));
-}
-
-function about($app)
-{
+};
+$about = function($app) {
     return $app->render(
         'about.html'
     );
-}
-
-$app->route('/about',about);
-$app->route('/',index);
-
+};
+$app->route('/about',$about);
+$app->route('/',$index);
 $app->run();
